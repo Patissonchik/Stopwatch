63f59572b45b8eeaf6d7ee2f325e3390258c6b0b
userc1 <userc1@itcube.win>
2019-12-02 17:51:42 +0300

    Stopwatching)))

---
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int seconds;
+    public void setSeconds(int value){
+        this.seconds = value;
+        updateTimeView();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSeconds(0);
+
+}
+    private CountDownTimer timer = new CountDownTimer(Long.MAX_VALUE, 1000) {
+
+        public void onTick(long millisUntilFinished) {
+            setSeconds(++seconds);
+        }
+
+        public void onFinish() {
+        }
+    };
+
+    public void onClickStart (View view){
+        timer.start();
+
+    }
+    public void onClickStop(View view){
+        timer.cancel();
+    }
+    public void onClickReset(View view){
+        timer.cancel();
+        setSeconds(0);
+    }
+    public void updateTimeView() {
+        int ss = seconds % 60;
+        int minutes = seconds / 60;
+        int mm = minutes % 60;
+        int hours = minutes / 60;
+        String timeString = String.format("%02d:%02d:%02d", hours, mm, ss);
+
+        TextView timeView = (TextView) findViewById(R.id.time);
+        timeView.setText(timeString);
+    }
+
+
+
+    }
+
+
