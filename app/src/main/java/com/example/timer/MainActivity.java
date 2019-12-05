f4e4044fe49dec8f7d0d7ab5ca0ed69bbb4c4ce8
patissonchik <bokov.es@mail.ru>
2019-12-05 17:42:01 +0300

    Lul OMEGA KEK

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.timer;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.PersistableBundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,13 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int seconds = (savedInstanceState != null)? savedInstanceState.getInt("seconds"):0;
-        setSeconds(seconds);
-        if(seconds!= 0){
-            timer.start();
-            Button button =(Button)findViewById(R.id.button);
-            button.setEnabled(false);
-        }
+        setSeconds(0);
+
 }
     private CountDownTimer timer = new CountDownTimer(Long.MAX_VALUE, 1000) {
 
@@ -42,20 +34,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickStart (View view){
         timer.start();
-        Button button =(Button)findViewById(R.id.button);
-        button.setEnabled(false);
 
     }
     public void onClickStop(View view){
         timer.cancel();
-        Button button =(Button)findViewById(R.id.button);
-        button.setEnabled(true);
     }
     public void onClickReset(View view){
         timer.cancel();
         setSeconds(0);
-        Button button =(Button)findViewById(R.id.button);
-        button.setEnabled(true);
     }
     public void updateTimeView() {
         int ss = seconds % 60;
@@ -68,11 +54,8 @@ public class MainActivity extends AppCompatActivity {
         timeView.setText(timeString);
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("seconds", seconds);
+
+
     }
-}
 
 
